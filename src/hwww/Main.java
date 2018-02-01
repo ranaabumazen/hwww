2de4ec922e6b2abc76f27333cb1072d86c815a9d
رنووش <@DESKTOP-3N5IKBA>
2018-02-01 21:09:05 +0200

    first commit

---
+++ src/hwww/Main.java
@@ -0,0 +1,18 @@
+package hwww;
+
+import hwww.CalculatorView;
+
+/**
+ *
+ * @author ahza
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new CalculatorView().setVisible(true);
+    }
+
+}
